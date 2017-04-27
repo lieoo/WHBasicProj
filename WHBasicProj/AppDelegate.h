c8d0408f03dc9aa5c600170c978ce5cbfbe93ccd
lieo <1006100679@qq.com>
2017-04-27 13:09:13 +0800

    init

---
+++ WHBasicProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHBasicProj
+//
+//  Created by 行政 on 17/4/27.
+//  Copyright © 2017年 lieo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHBasicProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHBasicProj
+//
+//  Created by 行政 on 17/4/27.
+//  Copyright © 2017年 lieo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
